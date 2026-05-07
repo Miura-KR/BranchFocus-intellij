067bc2a9973adf537bf63ae70d79452457c3daab
Miura-KR <ovis_w6v6_grappler95@yahoo.co.jp>
2026-05-07 23:44:50 +0900

    Tagに対応

--- src/main/kotlin/FocusBranchInLogAction.kt
+++ src/main/kotlin/FocusBranchInLogAction.kt
@@ -2,21 +2,31 @@ package com.k.pmpstudy
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
+import com.intellij.vcs.log.Hash
 import com.intellij.vcs.log.impl.VcsProjectLog
 import git4idea.GitBranch
-import git4idea.actions.branch.GitSingleBranchAction
+import git4idea.GitReference
+import git4idea.GitTag
+import git4idea.actions.ref.GitSingleRefAction
 import git4idea.repo.GitRepository
+import java.util.function.Supplier
 
-class FocusBranchInLogAction : GitSingleBranchAction() {
+class FocusBranchInLogAction : GitSingleRefAction<GitReference>(Supplier { "" }) {
     override fun actionPerformed(
         e: AnActionEvent,
         project: Project,
         repositories: List<GitRepository>,
-        reference: GitBranch,
+        reference: GitReference,
     ) {
         val (repo, hash) = repositories
-            .firstNotNullOfOrNull { r -> r.branches.getHash(reference)?.let { r to it } }
+            .firstNotNullOfOrNull { r -> hashOf(r, reference)?.let { r to it } }
             ?: return
         VcsProjectLog.showRevisionInMainLog(project, repo.root, hash)
+    }
+
+    private fun hashOf(repo: GitRepository, reference: GitReference): Hash? = when (reference) {
+        is GitBranch -> repo.branches.getHash(reference)
+        is GitTag -> repo.tagsHolder.state.value.tagsToCommitHashes[reference]
+        else -> null
     }
 }
